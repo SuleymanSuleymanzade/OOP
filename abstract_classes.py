cfa3ed118424c16795ef6dc692a6783bf0e04b07
Suleyman Suleymanzade <sul_soft@hotmail.com>
2020-02-17 13:41:22 +0400

    abstract methods

--- abstract_classes.py
+++ abstract_classes.py
@@ -2,7 +2,7 @@ import abc
 
 class Car(metaclass = abc.ABCMeta):
     def __init__(self):
-        self.coordinates = (0,0)
+        self._coordinates = (0,0)
     
     @abc.abstractmethod    
     def move(self, x, y):
@@ -13,17 +13,43 @@ class BMW(Car):
         super(BMW, self).__init__()
         self.model = model 
 
+
+    def __str__(self):
+        return f"{__class__.__name__} {self._coordinates}"
+
     def move(self, x, y):
-        self.coordinates = (self.coordinates[0] + x, self.coordinates[1] + y)
+        self._coordinates = (self._coordinates[0] + x, self._coordinates[1] + y)
 
+class Audi(Car):
+    def __init__(self, model):
+        super(Audi, self).__init__()
+        self.model = model 
+        self.__speed_coefficient = 3
 
-car = Car()
+    @property 
+    def speed_coefficient(self):
+        return self.__speed_coefficient 
 
-bmw = BMW("5-th series")
+    @speed_coefficient.setter 
+    def speed_coefficient(self, coef):
+        self.__speed_coefficient = coef 
 
-print(bmw.coordinates)
+    def __str__(self):
+        return f"{__class__.__name__} {self._coordinates}"
 
-bmw.move(2, 4)
+    def move(self, x, y):
+        self.coordinates = (self._coordinates[0] + x*self.__speed_coefficient,
+            self._coordinates[1] + y*self.__speed_coefficient)
 
-print(bmw.coordinates)
-
+def main():
+    bmw = BMW("5-th series")
+    audi = Audi('a4')
+    bmw.move(2, 4)
+    audi.move(2,4)
+    print(bmw)
+    print(audi)
+    audi.speed_coefficient = 7
+    audi.move(2,4)
+    print(audi)
+if __name__ == "__main__":
+    main()
